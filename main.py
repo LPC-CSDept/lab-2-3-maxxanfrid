404e33fedbbd4dcc08592d6bc13068f275750c02
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-11 20:47:51 -0800

    2-3

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ def main():
     val3 = int(input('Enter your number'))
     
     total = val1 + val2 + val3
-    print(f'The summation is {total:>20}')
+    print(f'Summation \t {total:>20}')
     
     avg = total / 3
-    print(f'The average is {avg:.2f}')
+    print(f'Average: \t {avg:.2f}')
     ##################################################
 
     pass
