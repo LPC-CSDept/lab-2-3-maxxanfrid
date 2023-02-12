b1a4483cb4cc03bef174867e6af60267658474e3
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-11 16:40:12 -0800

    2-3

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     ##################################################
     # Comlete your code here
+    val1 = int(input('Enter your number'))
+    val2 = int(input('Enter your number'))
+    val3 = int(input('Enter your number'))
+    sum = val1 + val2 + val3 
+    avg = sum/3 
+    print(sum)
+    print (avg)
     ##################################################
 
     pass
