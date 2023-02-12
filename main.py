fd00c1a25df576b3103dc56e5fd0371587cc3d9f
Madeleine Bruce <maxskyebruce@gmail.com>
2023-02-11 20:22:11 -0800

    2-3

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def main():
     var1 = int(input('Enter your number'))
     var2 = int(input('Enter your number'))
     var3 = int(input('Enter your number'))
-    total = var1 + var2 + var3 
+    
+    total = var1 + var2 + var3
     print(f'The summation is {total:>20}')
+    
     avg = total / 3
     print(f'The summation is {avg:.2f}')
     ##################################################
